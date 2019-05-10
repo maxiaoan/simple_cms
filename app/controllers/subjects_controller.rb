3d7f36a1425c901231ccb0b12349dceda66a42af
= <=>
2019-05-10 16:26:26 +0800

    Update Pages Sections Subjects controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -9,21 +9,57 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new(:name => "Default name")
+
   end
 
   def create
+
+    # Instantiate a new object form parameters
+    @subject = Subject.new(subject_params)
+    # Save the object
+    if @subject.save
+    # If save succeeds, redirect to the index action
+      flash[:notice] = "Subject created successfully."
+      redirect_to(subjects_path)
+    else
+    # if save fails, redisplay the form so the user can fix problems
+      render('new')
+    end
   end
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
   def update
+    # Find an object form parameters
+    @subject = Subject.find(params[:id])
+    # Update the object
+    if @subject.update_attributes(subject_params)
+    # If save succeeds, redirect to the show  action
+      flash[:notice] = "Subject updated successfully."
+      redirect_to(subject_path(@subject))
+    else
+    # if save fails, redisplay the form so the user can fix problems
+      render('edit')
+    end
   end
 
   def delete
+    @subject = Subject.find(params[:id])
   end
 
   def destroy
+    @subject = Subject.find(params[:id])
+    @subject.destroy
+    flash[:notice] = "Subject '#{@subject.name}'' destroyed successfully."
+    redirect_to(subjects_path)
   end
   
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
 Rails.application.routes.draw do
 
 
-  # get 'subjects/index'
-  # get 'subjects/show'
-  # get 'subjects/new'
-  # get 'subjects/edit'
-  # get 'subjects/delete'
   root 'demo#index'
 
+  # default there is no delete member
   resources :subjects do
     member do
+      get :delete
+    end
+  end
+
+  resources :pages do
+    member do
+      get :delete
+    end
+  end
+
+  resources :sections do
+    member do 
       get :delete
     end
   end
