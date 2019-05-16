2fec4cf6b133581da2bc1c35c39cb6816b696de4
= <=>
2019-05-16 15:57:18 +0800

    Update Controller filters to repeat useing code

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -2,10 +2,11 @@ class SubjectsController < ApplicationController
   
   layout 'admin'
 
+  before_action :set_subject_count, :only => [:new, :create, :edit, :update] 
+
   def index
     @subjects = Subject.sorted # stored definded in /suject.rb
-    #render('index')
-     
+    #render('index')    
   end
 
   def show
@@ -14,7 +15,6 @@ class SubjectsController < ApplicationController
 
   def new
     @subject = Subject.new(:name => "Default")
-    @subject_count = Subject.count + 1
   end
 
   def create
@@ -28,14 +28,12 @@ class SubjectsController < ApplicationController
       redirect_to(subjects_path)
     else
     # if save fails, redisplay the form so the user can fix problems
-      @subject_count = Subject.count
       render('new')
     end
   end
 
   def edit
     @subject = Subject.find(params[:id])
-    @subject_count = Subject.count
   end
 
   def update
@@ -48,7 +46,6 @@ class SubjectsController < ApplicationController
       redirect_to(subject_path(@subject))
     else
     # if save fails, redisplay the form so the user can fix problems
-      @subject_count = Subject.count
       render('edit')
     end
   end
@@ -68,5 +65,12 @@ class SubjectsController < ApplicationController
 
   def subject_params
     params.require(:subject).permit(:name, :position, :visible, :created_at)
+  end
+
+  def set_subject_count
+    @subject_count = Subject.count
+    if params[:action] == 'new' || params[:action] == 'create'
+      @subject_count += 1
+    end
   end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -11,9 +11,14 @@ class Section < ApplicationRecord
 
 	CONTENT_TYPES = ['text','HTML']
 
-	validates_presence_of :name
-	validates_length_of :name, :maximum => 255
+	# validates_presence_of :name
+	# validates_length_of :name, :maximum => 255
 	validates_inclusion_of :content_type, :in =>['text', 'HTML'], 
 	:message =>"must be one of: #{CONTENT_TYPES.join(', ')}"
-	validates_presence_of :content	
+	# validates_presence_of :content
+
+	validates :name, :presence => true,
+					 :length => {:maximum => 255}
+	validates :content, :presence => true
+
 end
