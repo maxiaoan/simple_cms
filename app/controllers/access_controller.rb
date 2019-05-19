98f4cbb93884e618fe529c74bc943ceda2cc8c19
maxiaoan <maxiaoansuccess@qq.com>
2019-05-19 16:05:02 +0800

    Display login name

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -5,7 +5,8 @@ class AccessController < ApplicationController
   before_action :confirm_logged_in, :except => [:login, :attempt_login, :logout]
 
   def menu
- 	# display text & links
+	 # display text & links
+	 @username = session[:username]
   end
 
   def login
@@ -22,7 +23,8 @@ class AccessController < ApplicationController
   	end
 
   	if authorized_user 
-  		session[:user_id] = authorized_user.id
+		  session[:user_id] = authorized_user.id
+		  session[:username] = authorized_user.username
   		flash[:notice] = "You are now logged in."
   		redirect_to(admin_path)
   	else
@@ -33,7 +35,8 @@ class AccessController < ApplicationController
   end
 
   def logout
-  	session[:user_id] = nil
+	  session[:user_id] = nil
+	  session[:username] = nil
   	flash[:notice] = 'logged out.'
   	redirect_to(access_login_path)
   end
